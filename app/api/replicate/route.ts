fddef337cff6ec0a8343601e41dedcebcec89d62
Vicente <33096355+Vicente015@users.noreply.github.com>
2025-08-06 16:12:29 +0100

    feat: wip minio storage connection

--- app/api/replicate/route.ts
+++ app/api/replicate/route.ts
@@ -1,3 +1,4 @@
+import { uploadImages } from "@/lib/minioClient";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
@@ -5,10 +6,11 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
 
+const MODEL = "black-forest-labs/flux-1.1-pro-ultra-finetuned"
+
 export async function POST(request: Request) {
   try {
-    const { prompt, model = "black-forest-labs/flux-1.1-pro-ultra-finetuned" } =
-      await request.json();
+    const { prompt } = await request.json();
 
     if (!prompt) {
       return NextResponse.json(
@@ -17,15 +19,11 @@ export async function POST(request: Request) {
       );
     }
     console.log("Received prompt:", prompt);
-    // Add RDTDOT activation keyword to the prompt
-    const enhancedPrompt = `RDTDOT ${prompt}`;
-
-    console.log("Starting Replicate generation...");
 
     const prediction = await replicate.predictions.create({
-      model: model,
+      model: MODEL,
       input: {
-        prompt: enhancedPrompt,
+        prompt: `RDTDOT ${prompt}`, // Add RDTDOT activation keyword to the prompt
         finetune_id: "ef2a1a03-c2d2-4b23-bd94-ae0cf1609f0f",
         num_outputs: 1,
         aspect_ratio: "1:1",
@@ -33,28 +31,17 @@ export async function POST(request: Request) {
         output_quality: 80,
       },
     });
-
     console.log("Prediction created:", prediction.id);
 
-    // Wait for the prediction to complete
-    let finalPrediction = prediction;
-    while (
-      finalPrediction.status !== "succeeded" &&
-      finalPrediction.status !== "failed"
-    ) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      finalPrediction = await replicate.predictions.get(prediction.id);
-      console.log("Prediction status:", finalPrediction.status);
-    }
-
-    if (finalPrediction.status === "failed") {
-      throw new Error("Prediction failed: " + finalPrediction.error);
-    }
-
-    console.log("Final prediction output:", finalPrediction.output);
-    return NextResponse.json({ output: finalPrediction.output });
+    if (prediction.error) throw new Error('Prediction failed')
+    const result = await replicate.wait(prediction)
+    const image: string | null = result.output
+    if (!image) throw new Error('No output received')
+    const imageOnMinio =await uploadImages([image])
+    return NextResponse.json({ output: imageOnMinio });
   } catch (error) {
     console.error("Replicate API error:", error);
+
     return NextResponse.json(
       { error: "Failed to generate image" },
       { status: 500 }
---
+++ lib/minioClient.ts
@@ -0,0 +1,101 @@
+import 'server-only'
+
+import fs from 'node:fs'
+import { tmpdir } from 'node:os'
+import path from 'node:path'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+import pRetry from 'p-retry'
+import { Client } from 'minio'
+
+type ObjectMetaData = Record<string, string | number>
+
+if (!process.env.MINIO_DOMAIN) throw new Error('Missing MINIO_DOMAIN in env variables')
+
+const BUCKET = 'vision-images' as const
+const streamPipeline = promisify(pipeline)
+
+export function getObjectUrl(objectName: string): string {
+  return `https://${process.env.MINIO_DOMAIN}/${BUCKET}/${objectName}`
+}
+
+const minioClient = new Client({
+  endPoint: process.env.MINIO_DOMAIN,
+  port: 80,
+  useSSL: false,
+  accessKey: process.env.MINIO_ACCESS_KEY,
+  secretKey: process.env.MINIO_SECRET_KEY,
+})
+
+export class Minio_Error extends Error {
+  constructor(message: string | undefined) {
+    super(message)
+    this.name = 'Minio_Error'
+  }
+}
+
+/**
+ * Uploads an image to bucket
+ * @param sourcePath
+ * @param destinationName hash/part.webp
+ * @param metadata
+ */
+async function uploadImage(sourcePath: string, destinationName: string, metadata: ObjectMetaData) {
+  console.debug('Uploading image', sourcePath, destinationName)
+  try {
+    console.debug('uploading', sourcePath, destinationName, metadata)
+    const result = await minioClient.fPutObject(BUCKET, destinationName, sourcePath, metadata)
+    if (!result) throw new Error('No result')
+    console.debug('✅ Uploaded image successfully!', result)
+    return result
+  } catch (error) {
+    console.error('Minio_Error', error)
+    throw new Minio_Error(`File upload error: ${error}`)
+  }
+}
+
+async function downloadImageTmp(tempDir: string, originURL: string, destinationName: string) {
+  try {
+    const response = await fetch(originURL)
+    if (!response.ok) throw new Minio_Error(`Failed to fetch ${originURL}, ${response.status}`)
+
+    const filePath = path.join(tempDir, destinationName)
+    const fileStream = fs.createWriteStream(filePath)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await streamPipeline(response.body as any , fileStream)
+    console.debug('✅ Downloaded image', filePath)
+    return filePath
+  } catch (error) {
+    console.error(error)
+    throw new Minio_Error(`File download error: ${error}`)
+  }
+}
+
+async function downloadImage(tempDir: string, originURL: string, destinationName: string) {
+  const filePath = await downloadImageTmp(tempDir, originURL, destinationName)
+  await uploadImage(filePath, destinationName, {})
+  return getObjectUrl(destinationName)
+}
+
+async function uploadImages(images: string[]): Promise<string[]> {
+  const tempDir = fs.mkdtempSync(path.join(tmpdir(), crypto.randomUUID()))
+
+  const result = await Promise.all(images.map(async (imageURL) => await pRetry(
+    async () => await downloadImage(tempDir, imageURL, imageURL.split('/').at(-1)!),
+    {
+      retries: 6,
+      minTimeout: 10,
+      maxTimeout: 200,
+      onFailedAttempt: (error) => {
+        console.debug(`🔄 Failed attempt ${error.attemptNumber} to download ${imageURL}`)
+      },
+    },
+  )))
+
+  console.debug(`✅ Uploaded ${images.length} images`)
+  return result
+}
+
+export {
+  uploadImages
+}
