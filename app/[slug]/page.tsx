0a43b1f139734dc0665a3708ccad12cad30fbd0f
Vicente <33096355+Vicente015@users.noreply.github.com>
2025-08-26 10:37:29 +0100

    fix: navbar in layout again, fix missing navbar issue

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { PageBuilder } from "@/components/PageBuilder";
-import Navbar from "@/components/ui/navbar";
 import { sanityFetch } from "@/sanity/lib/live";
-import { NAVBAR_QUERY, PAGE_QUERY } from "@/sanity/lib/queries";
+import { PAGE_QUERY } from "@/sanity/lib/queries";
 
 export default async function Page({
   params,
@@ -13,24 +12,11 @@ export default async function Page({
     params: await params,
   });
 
-  const { data: navbar } = await sanityFetch({
-    query: NAVBAR_QUERY,
-  });
-
-
   return (
-    <>
-      {navbar && (
-        <Navbar
-          {...navbar}
-        />
-      )}
-
-      <main>
-        {page?.content ? (
-          <PageBuilder content={page?.content} />
-        ) : null}
-      </main>
-    </>
+    <main>
+      {page?.content ? (
+        <PageBuilder content={page?.content} />
+      ) : null}
+    </main>
   );
 }
