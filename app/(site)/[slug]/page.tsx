805cc7849a16133731d622e5e0f540a9b01b8652
efont25 <79472270+efont25@users.noreply.github.com>
2025-09-12 15:36:29 +0100

    Fix

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,17 +1,11 @@
-import { defineQuery } from "next-sanity";
 import { PageBuilder } from "@/components/PageBuilder";
 import { sanityFetch } from "@/sanity/lib/live";
 import { PAGE_QUERY } from "@/sanity/lib/queries";
 import NotFound from "@/components/ui/not-found";
 
-type Params = { slug: string };
-
-export default async function Page({ params }: { params: Params }) {
-  const { data: page } = await sanityFetch({
-    query: PAGE_QUERY,
-    params,
-  });
-
+export default async function Page(props: any) {
+  const { params } = props as { params: { slug: string } };
+  const { data: page } = await sanityFetch({ query: PAGE_QUERY, params });
   return (
     <main>
       {page?.content ? <PageBuilder content={page.content} /> : <NotFound />}
